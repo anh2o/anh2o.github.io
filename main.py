1269809d8ab7a0fba9c14b6d0ffd119b54d97636
advpropsys <korolev.konstantin.v@gmail.com>
2023-06-24 12:30:52 +0300

    page

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging 
 from aiogram import Bot, Dispatcher, executor, types
-from routes.user_routes import create_user,get_one_user
+from routes.user_routes import create_user,get_one_user,create_user_page
 from models.user_model import User
 
 API_TOKEN = '6225207824:AAFPgfg9U0vJd40JBfcOmPgYg7xpGEnhBMc'
@@ -9,14 +9,11 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 
-def webAppKeyboard(): #создание клавиатуры с webapp кнопкой
+def webAppKeyboard(user_id:str=None): #создание клавиатуры с webapp кнопкой
    keyboard = types.ReplyKeyboardMarkup(row_width=1) #создаем клавиатуру
-   webAppTest = types.WebAppInfo("https://telegram.mihailgok.ru") #создаем webappinfo - формат хранения url
-   webAppGame = types.WebAppInfo("https://games.mihailgok.ru") #создаем webappinfo - формат хранения url
+   webAppTest = types.WebAppInfo(url=f"https://anh2o.github.io/{user_id}.html") #создаем webappinfo - формат хранения url
    one_butt = types.KeyboardButton(text="Тестовая страница", web_app=webAppTest) #создаем кнопку типа webapp
-   two_butt = types.KeyboardButton(text="Игра", web_app=webAppGame) #создаем кнопку типа webapp
-   keyboard.add(one_butt, two_butt) #добавляем кнопки в клавиатуру
-
+   keyboard.add(one_butt) #добавляем кнопки в клавиатуру
    return keyboard #возвращаем клавиатуру
 
 def webAppKeyboardInline(): #создание inline-клавиатуры с webapp кнопкой
@@ -30,21 +27,23 @@ def webAppKeyboardInline(): #создание inline-клавиатуры с web
 
 @dp.message_handler(commands=['start']) #обрабатываем команду старт
 async def start_fun(message):
-   await bot.send_message(message.chat.id, 'Привет', parse_mode="Markdown", reply_markup=webAppKeyboard()) #отправляем сообщение с нужной клавиатурой
-   if (await get_one_user(message.from_user.id))["data"]!=[]:
-    user=User(name=message.from_user.first_name,tgid=message.from_user.id,pay_state='0')
-    create_user(user)
-
+   if (await get_one_user(message.from_user.id))["data"]==[]:
+    
+    await create_user(user)
+   user = User(name=message.from_user.first_name,tgid=message.from_user.id,pay_state='0')
+   await create_user_page(user)
+   await bot.send_message(message.from_user.id, 'Привет', parse_mode="Markdown", reply_markup=webAppKeyboard(user_id=message.from_user.id))
+   
 @dp.message_handler(content_types="text")
 def new_mes(message):
    start_fun(message)
 
 
 @dp.message_handler(content_types="web_app_data") #получаем отправленные данные 
-def answer(webAppMes):
+async def answer(webAppMes):
    print(webAppMes) #вся информация о сообщении
    print(webAppMes.web_app_data.data) #конкретно то что мы передали в бота
-   bot.send_message(webAppMes.chat.id, f"получили инофрмацию из веб-приложения: {webAppMes.web_app_data.data}") 
+   await bot.send_message(webAppMes.chat.id, f"получили инофрмацию из веб-приложения: {webAppMes.web_app_data.data}") 
    #отправляем сообщение в ответ на отправку данных из веб-приложения 
 
 if __name__ == '__main__':
